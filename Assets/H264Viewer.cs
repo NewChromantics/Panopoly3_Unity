ec113afd6f64a8f174c9dee3acbc05beb6ded149
Graham Reeves <graham@grahamreeves.com>
2020-08-20 14:06:42 +0100

    Added h264 decoder & linked together

---
+++ Assets/H264Viewer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class H264Viewer : MonoBehaviour
+{
+	PopH264.Decoder Decoder;
+	public PopH264.DecoderMode DecoderMode = PopH264.DecoderMode.MagicLeap_NvidiaHardware;
+	public bool ThreadedDecoding = true;
+	int FrameCounter = 0;
+	List<Texture2D> FramePlaneTextures;
+	List<Pop.PixelFormat> FramePlaneFormats;
+
+	public void DecodeH264Data(byte[] Data)
+	{
+		if (Decoder == null)
+			Decoder = new PopH264.Decoder(DecoderMode, ThreadedDecoding);
+
+		Decoder.PushFrameData(Data, FrameCounter++);
+	}
+
+	void Update()
+	{
+		if (Decoder != null)
+			UpdateFrame();
+	}
+
+	void UpdateFrame()
+	{
+		var NewFrameNumber = Decoder.GetNextFrame(ref FramePlaneTextures, ref FramePlaneFormats);
+		if (!NewFrameNumber.HasValue)
+			return;
+
+		Debug.Log("New frame " + NewFrameNumber.Value);
+
+		//	update material
+	}
+}
