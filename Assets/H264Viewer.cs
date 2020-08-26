003648646547bf0725d0d674162bb3f9e1c821e4
Graham Reeves <graham@grahamreeves.com>
2020-08-26 17:09:10 +0100

    Now splitting streams in one file to two different decoders

--- Assets/H264Viewer.cs
+++ Assets/H264Viewer.cs
@@ -13,13 +13,28 @@ public class H264Viewer : MonoBehaviour
 	List<Texture2D> FramePlaneTextures;
 	List<Pop.PixelFormat> FramePlaneFormats;
 	public List<string> TextureUniformNames;
+	public string StreamFilter;
 
 	PopCapFrameMeta LastMeta;
+	PopCapFrameMeta LastStreamMeta;
+
 
 	//	todo: keep meta associated with frame number here
 	PopCapFrameMeta GetMeta(int FrameNumber)
 	{
-		return LastMeta;
+		return LastStreamMeta;
+	}
+
+	bool IsLastMetaForThisStream()
+	{
+		if (string.IsNullOrEmpty(StreamFilter))
+			return true;
+		if (LastMeta == null)
+			return true;
+		if (LastMeta.Stream != StreamFilter)
+			return false;
+
+		return true;
 	}
 
 	public void OnMeta(string MetaJson)
@@ -29,13 +44,22 @@ public class H264Viewer : MonoBehaviour
 		//	in decode;
 		//	this.FrameMeta[FrameCounter] = LastMeta
 		var NewMeta = JsonUtility.FromJson<PopCapFrameMeta>(MetaJson);
+		LastMeta = NewMeta;
 
-		if (NewMeta != null)
-			LastMeta = NewMeta;
+		//	apply filter
+		if (IsLastMetaForThisStream())
+			LastStreamMeta = LastMeta;
 	}
 
 	public void DecodeH264Data(byte[] Data)
 	{
+		//	skip data which is destined for another stream
+		if (!IsLastMetaForThisStream())
+		{
+			Debug.Log("skipping frame; is for stream " + this.LastMeta.Stream);
+			return;
+		}
+
 		if (Decoder == null)
 			Decoder = new PopH264.Decoder(DecoderMode, ThreadedDecoding);
 
